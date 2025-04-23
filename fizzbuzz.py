c7c8d39e697cbe1366574678db41e63fd14aa91a
coevol <rpajhc@gmail.coml>
2025-04-23 17:35:58 +0900

    feat: Create fibonacci sequence

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,3 +2,13 @@ print("Hello~ FiveCoders It Works")
 for i in range(1, 20):
     print(f"{i}")
 
+
+# Fibonacci Sequence by @github/coevol
+print("Recurrent Fibonacci Sequence")
+print("Enter fibo(number) with a number")
+def fibo(n:int):
+    if n < 2:
+        return n
+    else:
+        return fibo(n-1) + fibo(n-2)
+
